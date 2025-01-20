b150dbabe682c52c4bd250dc666845f8489d48c7
Thanapon Phorarmart <91049342+ntdotjsx@users.noreply.github.com>
2025-01-21 03:12:29 +0700

    Update sketch_jun14b.ino

--- sketch_jun14b.ino
+++ sketch_jun14b.ino
@@ -2,12 +2,8 @@
 #include <DigiKeyboard.h>
 
 void setup() {
-    DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // กด Windows + R
+    DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); 
     DigiKeyboard.delay(100);
-    DigiKeyboard.print("cmd /c start https://github.com/ntdotjsx"); // พิมพ์ "cmd /K whoami" (ใช้ /K แทน /C)
-    DigiKeyboard.sendKeyStroke(KEY_ENTER); // กด Enter
-}
-
-void loop() {
-    // ไม่ต้องมีการใช้งานใน loop ในกรณีนี้
+    DigiKeyboard.print("cmd /c start https://github.com/ntdotjsx"); 
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
